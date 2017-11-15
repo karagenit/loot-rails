ca227219e2d7e4bd1fafba45e8c93c92b563dd25
Caleb Smith <karagenit@outlook.com>
2017-11-14 20:20:54 -0500

    Add Master Actions

--- app/assets/javascripts/master.js
+++ app/assets/javascripts/master.js
@@ -8,7 +8,8 @@ function masterNew() {
         return;
     }
 
-    var row = table.insertRow(table.rows.length);
+    var index = table.rows.length
+    var row = table.insertRow(index);
     var cname = row.insertCell(0);
     var cqty = row.insertCell(1);
     var cvalue = row.insertCell(2);
@@ -16,4 +17,14 @@ function masterNew() {
     cname.innerHTML = name;
     cqty.innerHTML = qty;
     cvalue.innerHTML = value;
+    cactions.innerHTML = `
+        <span class="input-group">
+            <button type="button" class="btn btn-primary btn-table"
+                onclick="masterTake(${index})">Take</button>
+            <button type="button" class="btn btn-warning btn-table"
+                onclick="masterSell(${index})">Sell</button>
+            <button type="button" class="btn btn-danger btn-table"
+                onclick="masterDelete(${index})">Delete</button>
+        </span>
+    `;
 }
