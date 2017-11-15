e7510dee4141755f7d84082373317aed9f2c8a5b
Caleb Smith <karagenit@outlook.com>
2017-11-14 19:55:06 -0500

    Add Qty/Value to Master New

--- app/assets/javascripts/master.js
+++ app/assets/javascripts/master.js
@@ -1,10 +1,14 @@
 function masterNew() {
     var name = $("#master-new-name")[0].value;
+    var qty = $("#master-new-qty")[0].value;
+    var value = $("#master-new-value")[0].value;
     var table = $("#master")[0];
-    var row = table.insertRow(0); //TODO use the end
+    var row = table.insertRow(table.rows.length);
     var cname = row.insertCell(0);
     var cqty = row.insertCell(1);
     var cvalue = row.insertCell(2);
     var cactions = row.insertCell(3); //TODO add buttons to this
     cname.innerHTML = name;
+    cqty.innerHTML = qty;
+    cvalue.innerHTML = value;
 }
